5dff91fe26c6883351fa77daa1760fd205eb808a
Mitesh Gosar <mitesh.gosar@lr.org>
2020-04-12 19:18:47 +0530

    Add project files.

---
+++ DevOpsDemo/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using DevOpsDemo.Models;
+using DevOpsDemo.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DevOpsDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPostRepository _postRepository;
+
+        public HomeController(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _postRepository.GetPosts();
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
